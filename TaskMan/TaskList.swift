2b1a58281af3000b734cf0809b4e1dfa784260dd
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2016-12-05 15:08:50 -0200

    Removing creation/update and name fields from TaskList

--- TaskMan/TaskList.swift
+++ TaskMan/TaskList.swift
@@ -19,20 +19,9 @@ struct TaskList {
     /// List of task segments registered for the tasks above
     var taskSegments: [TaskSegment] = []
     
-    /// Display name for this task list
-    var name: String
-    
-    /// Datetime at which this task list was created
-    var creationDate: Date
-    
-    /// Last date at which this task list was modified
-    var updateDate: Date
-    
-    /// Convenience initializer that inits an empty task list, with a creation/update date set to now
-    init(name: String) {
-        self.name = name
-        self.creationDate = Date()
-        self.updateDate = self.creationDate
+    /// Convenience initializer that inits all fields of this model empty
+    init() {
+        
     }
 }
 
@@ -42,9 +31,6 @@ extension TaskList: JsonInitializable, JsonSerializable {
     init(json: JSON) throws {
         try tasks = json[JsonKey.tasks].tryParseModels()
         try taskSegments = json[JsonKey.taskSegments].tryParseModels()
-        try name = json[JsonKey.name].tryString()
-        try creationDate = json[JsonKey.creationDate].tryParseDate(withFormatter: rfc3339DateTimeFormatter)
-        try updateDate = json[JsonKey.updateDate].tryParseDate(withFormatter: rfc3339DateTimeFormatter)
     }
     
     func serialize() -> JSON {
@@ -52,9 +38,6 @@ extension TaskList: JsonInitializable, JsonSerializable {
         
         dict[.tasks] = tasks.jsonSerialize().map { $0.object }
         dict[.taskSegments] = taskSegments.jsonSerialize().map { $0.object }
-        dict[.name] = name
-        dict[.creationDate] = rfc3339StringFrom(date: creationDate)
-        dict[.updateDate] = rfc3339StringFrom(date: updateDate)
         
         return dict.mapToJSON()
     }
@@ -66,9 +49,6 @@ extension TaskList {
     enum JsonKey: String, JSONSubscriptType {
         case tasks
         case taskSegments = "task_segments"
-        case name
-        case creationDate = "creation_date"
-        case updateDate = "update_date"
         
         var jsonKey: JSONKey {
             return JSONKey.key(self.rawValue)
